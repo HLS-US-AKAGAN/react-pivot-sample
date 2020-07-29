380d728ccf7c39c51d28eedb1b7b6d63dac421d7
akagan-hls <57148258+akagan-hls@users.noreply.github.com>
2020-07-29 16:47:24 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PivotTableUI from 'react-pivottable/PivotTableUI';
+import 'react-pivottable/pivottable.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [ ['name', 'gender', 'house', 'age'],
+['Jon', 'm', 'Stark', 14],
+['Arya', 'f', 'Stark', 10],
+['Cersei', 'f', 'Baratheon', 38],
+['Tywin', 'm', 'Lannister', 67],
+['Tyrion', 'm', 'Lannister', 34],
+['Joffrey', 'm', 'Baratheon', 18],
+['Bran', 'm', 'Stark', 8],
+['Jaime', 'm', 'Lannister', 32],
+['Sansa', 'f', 'Stark', 12]];
+
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = props;
+  }
+
+  render() {
+      return (
+          <PivotTableUI
+              data={data}
+              onChange={s => this.setState(s)}
+              {...this.state}
+          />
+      );
+  }
 }
 
 export default App;
